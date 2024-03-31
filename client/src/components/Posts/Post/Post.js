af636ee3cf971b0ef2b7aafc5aaa15dcddc02f9d
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2024-04-01 02:56:11 +0530

    installed @material-ui/icons

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
-// import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+
 import useStyles from './styles';
 const Post = () => {
     const classes = useStyles();
