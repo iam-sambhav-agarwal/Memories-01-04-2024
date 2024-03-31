c22061f7be8940874f72995922315aec22a90752
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2024-04-01 02:44:29 +0530

    server and client

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import {thunk} from 'redux-thunk';
+import ReactDOM from 'react-dom';
+import reducers from './reducers';
+
+import App from './App';
+
+const store = configureStore({
+    reducer: reducers,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+  });
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+  </ Provider>,document.getElementById('root'));
