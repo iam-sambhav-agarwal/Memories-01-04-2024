7b48cc002e3118bed07c07e6366376a50d21c8f4
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2024-04-01 16:44:19 +0530

    Add Post component with card layout and styling

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import {thunk} from 'redux-thunk';
+import './index.css';
 import ReactDOM from 'react-dom';
 import reducers from './reducers';
 
