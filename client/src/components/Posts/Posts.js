0268a20e4343c153cb7fd13c035abecae5ac99d8
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2024-04-02 19:55:18 +0530

    Add updatePost action and functionality

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,7 +4,7 @@ import {useSelector} from 'react-redux';
 import useStyles from './styles';
 import { CircularProgress, Grid } from '@material-ui/core';
 
-const Posts = () => {
+const Posts = ({setCurrentId}) => {
   const posts = useSelector((state) => state.posts);
   console.log(posts);
 const classes = useStyles();
@@ -13,7 +13,7 @@ const classes = useStyles();
         <Grid className={classes.container} container alignItems='stretch' spacing={3}>
             {posts.map((post) => (  
                <Grid key={post._id} item xs={12} sm={6}>
-                   <Post post={post}/> 
+                   <Post post={post}setCurrentId={setCurrentId}/> 
                 </Grid>
             ))  
               }
