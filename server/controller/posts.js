5d4b12bcbf9a4f25857d7de480bf7631f37b6584
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2024-04-05 17:00:22 +0530

    add NavBar component and styles

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,6 +1,5 @@
 import PostMessage from '../models/postMessage.js';
 import mongoose from 'mongoose';
-// import PostMessage from '../models/postMessage.js';
 export const getPosts = async (req, res) => {
 
     try {
@@ -14,15 +13,15 @@ export const getPosts = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-        const body = req.body;
-        const newPost = new PostMessage(body);
-        try {
-            await newPost.save();
-            res.status(201).json(newPost);
-        } catch (error) {
-            res.status(409).json({ message: error.message });
-        }
-};  
+    const body = req.body;
+    const newPost = new PostMessage(body);
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+};
 
 export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
@@ -30,9 +29,9 @@ export const updatePost = async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
     const updatedPost = await PostMessage.findByIdAndUpdate( _id, { ...post, _id }, { new: true });
     res.json(updatedPost);
-} 
+}
 
-export const deletePost = async (req, res) => { 
+export const deletePost = async (req, res) => {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
     await PostMessage.findByIdAndDelete(id);
