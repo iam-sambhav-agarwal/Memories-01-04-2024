aa36521eae30d7722300f7fe6b223bd743d027b9
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2024-04-07 19:50:23 +0530

    Refactor routing in App.js and fix formatting in Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { Container, } from '@material-ui/core';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-import {BrowserRouter , Route, Switch} from 'react-router-dom';
 
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <Router>
             <Container maxwidth='lg'>
                 <NavBar />
-                <Switch>
+                <Routes>
                    <Route path='/' exact component={Home} />
                    <Route path='/auth' exact component={Auth} />
-                </Switch>
-                <Home />
+                </Routes>
             </Container>
-        </ BrowserRouter>
+        </ Router>
 
     )
 }
