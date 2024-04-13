cf56ef1f2e1b173faa36a30367defb146abf3720
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2024-04-14 01:48:49 +0530

    Add Auth reducer and update combineReducers in index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
 import posts from "./posts";
+import Auth from "./Auth";
 
-
-export default combineReducers({ posts });
+export default combineReducers({ posts,Auth });
