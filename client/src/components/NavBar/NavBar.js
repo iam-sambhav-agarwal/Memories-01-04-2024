5262dcb92fbc96b36e2fa48c1ef41d5689be04e6
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2024-04-22 23:39:13 +0530

    Refactor Form component to handle user authentication and update post creation

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,18 +1,21 @@
-import React, { useState,useEffect } from "react";
-import { Link, useNavigate,useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import memories from "../../images/memories.png";
 import { AppBar, Toolbar, Typography, Avatar, Button } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import useStyles from "./styles";
+import {jwtDecode} from 'jwt-decode'
 
 
 
 const NavBar = () => {
   const classes = useStyles();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
+  
   const dispatch = useDispatch();
   const navigate = useNavigate()
   const location = useLocation()
+
 
   const logout = () => {
     dispatch({ type: 'LOGOUT' })
@@ -20,13 +23,15 @@ const NavBar = () => {
     setUser(null)
   }
 
-
   useEffect(() => {
-    const token = user?.token;
+    const token = user?.token; 
 
+    if (token) {
+      const decodedToken = jwtDecode(token)
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout()
+    }    
     setUser(JSON.parse(localStorage.getItem('profile')))
   }, [location])
-
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
