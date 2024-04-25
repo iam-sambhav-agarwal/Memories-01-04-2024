e8fa9e8d5b191b63a027b35780676c7966cfcad3
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2024-04-25 16:33:53 +0530

    added postdetails comonent

---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PostDetails =()=>{
+
+    console.log('POST DETAILS')
+    return (
+        <div>
+            <h1>POST DETAILS</h1>
+        </div>
+    )  
+}
+
+export default PostDetails
