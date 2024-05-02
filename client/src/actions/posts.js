985422676eb3e7f05ae64287b7a006dbc48d21c4
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2024-05-03 02:14:50 +0530

    added stat & end loading funtionality

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,12 @@
 import * as api from '../api'
 
 //action creators
-export const getPosts = () => async (dispatch) => {
+export const getPosts = (page) => async (dispatch) => {
     try {
-        const { data } = await api.fetchPosts();
+        dispatch({ type: 'START_LOADING' })
+        const { data } = await api.fetchPosts(page);
         dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: 'END_LOADING' })
     } catch (error) {
         console.log(error);
     }
@@ -12,19 +14,23 @@ export const getPosts = () => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
+        dispatch({ type: 'START_LOADING' })
         const { data: { data } } = await api.fetchPostsBySearch(searchQuery);
         dispatch({ type: 'FETCH_BY_SEARCH', payload: data });
-        
+        dispatch({ type: 'END_LOADING' })
     } catch (error) {
         console.log(error);
     }
 
 }
 
-export const createPost = (post) => async (dispatch) => {
+export const createPost = (post, navigate) => async (dispatch) => {
     try {
+        dispatch({ type: 'START_LOADING' })
         const { data } = await api.createPost(post);
         dispatch({ type: 'CREATE', payload: data });
+        navigate.push(`/posts/${data._id}`);
+        dispatch({ type: 'END_LOADING' })
     } catch (error) {
         console.log(error);
     }
